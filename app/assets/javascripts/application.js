3e10822b15c34222e39e3ceacf216aa93e9a3aeb
wubwubwubwub <nolan.camp@gmail.com>
2015-11-03 10:39:53 -0500

    add leaflet map

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require leaflet
 //= require_tree .
 
 
@@ -41,4 +42,21 @@ $(function(){
         });
         return false;
     });
+    var address = "<div id='marker-title'>Thump Recording</div>295 Eckford St<br>Brooklyn, NY 11222"
+    var lon = $('#map').data("lon");
+    var lat = $('#map').data("lat");
+    var map = L.map('map').setView([lon, lat], 14);
+    
+    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+        // attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+        maxZoom: 14,
+        id: 'nolancamp.cigjhgpyc002zuikruj5roc05',
+        accessToken: 'pk.eyJ1Ijoibm9sYW5jYW1wIiwiYSI6ImNpZ2poZ3I0OTAwNGR0dGx1MDlzM3Vqb3gifQ.wXiBFbtGd4LpdZG0J9MVWQ'
+    }).addTo(map);
+
+    L.marker([lon, lat]).addTo(map)
+        .bindPopup(address);
+    // .openPopup();
+    
+    
 })

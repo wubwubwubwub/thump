7ee453cd0727e466bce38d2dcddf189a0c3d4d03
Nolan Camp <nolan.camp@gmail.com>
2015-12-10 23:36:54 -0500

    order equipment queries

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -11,10 +11,10 @@ class PagesController < ApplicationController
   end
 
   def gear
-    @categories = EquipmentCategory.includes(:equipment).studio
+    @categories = EquipmentCategory.includes(:equipment).studio.order('equipment_categories.name, equipment.name ASC')
   end
 
   def field
-    @categories = EquipmentCategory.includes(:equipment).field
+    @categories = EquipmentCategory.includes(:equipment).field.order('equipment_categories.name, equipment.name ASC')
   end
 end

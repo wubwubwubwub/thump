6add019032fe4a78ebba1ad606119f4945c6a82c
Nolan Camp <nolan.camp@gmail.com>
2015-12-17 10:49:10 -0500

    update gear lists to display first child on load

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -23,43 +23,20 @@ function capitalizeFirstLetter(string) {
 }
 
 $(document).ready(function(){    
-
-    $(".gear-categories").find("a").filter(".link").each(function() {
-        $(this).on('click', function() {
-            event.preventDefault();
-            $(".welcome-container").fadeOut();
-            $(".gear-categories").find(".link").css("color", "#d0d0d0");
-            $(this).css("color", "#ff5656");
-            
-            var category = $(this).data('category');
-            $(".gear-categories").find(".gear-list").each(function() {
-                if ($(this).data('category') == category) {
-                    $(this).fadeIn();
-                } else {
-                    $(this).fadeOut();
-                };
-            });
-        });
-    });
-
     $('a.gear-photo-link').featherlight({
         loading: '/assets/loading.gif'
     });
-
-    // to make stool pic exact height of window minus header
-    var windowHeight = $(window).height();
-    $('.profiled-pic').css("height", windowHeight);
-    // var windowHeightPlus = ($(window).height() + 100);
-    $('.studio-pic').css("height", windowHeight-84);
-
+    // // to make stool pic exact height of window minus header
+    // var windowHeight = $(window).height();
+    // $('.profiled-pic').css("height", windowHeight);
+    // // var windowHeightPlus = ($(window).height() + 100);
+    // $('.studio-pic').css("height", windowHeight-84);
 });
 
 $(document).on("page:change", function() {
-
     if (window.location.pathname.split("/")[1] != "") {
         $("title").html("Thump | " + capitalizeFirstLetter(window.location.pathname.split("/")[1]));
     };
-
     $('header').find('a').each(function() {
         if ($(this).attr('href').split('/')[1] === window.location.pathname.split('/')[1]) {
             $(this).addClass('current');
--- app/assets/javascripts/pages.coffee
+++ app/assets/javascripts/pages.coffee
@@ -1,3 +1,24 @@
 # Place all the behaviors and hooks related to the matching controller here.
 # All this logic will automatically be available in application.js.
 # You can use CoffeeScript in this file: http://coffeescript.org/
+
+$(document).on "page:change", ->
+  
+  $('.gear-categories').find('a').filter('.link').each ->
+    $(this).on 'click', ->
+      event.preventDefault()
+      # $(".welcome-container").fadeOut();
+      $('.gear-categories').find('.link').css 'color', '#d0d0d0'
+      $(this).css 'color', '#ff5656'
+      category = $(this).data('category')
+      $('.gear-categories').find('.gear-list').each ->
+        if $(this).data('category') == category
+          $(this).fadeIn()
+        else
+          $(this).fadeOut()
+        return
+      return
+    return
+
+  $('.gear-categories').find('a').first().click()
+  $('.gear-categories').find('div').first().css 'display', 'block'
